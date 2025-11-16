fb37fa95ad895ca50f54cf31e0e28dadc9cb12cc
gethsemane-rb <gethsemane.rb@gmail.com>
2025-11-16 12:28:04 +0530

    Testing video

--- src/app/components/home-bg-video-loader/home-bg-video-loader.component.ts
+++ src/app/components/home-bg-video-loader/home-bg-video-loader.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-bg-video-loader',
@@ -7,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrl: './home-bg-video-loader.component.scss'
 })
 export class HomeBgVideoLoaderComponent {
+
+ @ViewChild('heroVideo') heroVideo!: ElementRef<HTMLVideoElement>;
+
+   playlist: string[] = [
+     '/assets/video/bg_video1.mp4',
+     '/assets/video/bg_video2.mp4',
+     '/assets/video/bg_video3.mp4'
+   ];
+
+   index: number = 0;
+
+   ngAfterViewInit(): void {
+     const video = this.heroVideo.nativeElement;
+     video.src = this.playlist[this.index];
+
+     video.onended = () => {
+       this.index++;
+       if (this.index < this.playlist.length) {
+         video.src = this.playlist[this.index];
+         video.play();
+       } else {
+         // restart from first video (loop behavior)
+         this.index = 0;
+         video.src = this.playlist[this.index];
+         video.play();
+       }
+     };
+   }
+
 
 }
