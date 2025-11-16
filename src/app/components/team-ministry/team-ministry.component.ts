d0a3e5b339a704cf027957b38d2abc75124afc8e
gethsemane-rb <gethsemane.rb@gmail.com>
2025-11-16 11:16:09 +0530

    Updated About us

--- src/app/components/team-ministry/team-ministry.component.ts
+++ src/app/components/team-ministry/team-ministry.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-team-ministry',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './team-ministry.component.html',
   styleUrl: './team-ministry.component.scss'
 })
 export class TeamMinistryComponent {
-
+ ministries = [
+    {
+      name: 'Mahesh',
+      role: 'Worship Ministry',
+      image: 'assets/images/people/mahesh.png'
+    },
+    {
+      name: 'Roshan',
+      role: 'Worship Ministry',
+      image: 'assets/images/people/roshan.png'
+    },
+    {
+      name: 'Brijesh',
+      role: 'Kids Ministry',
+      image: 'assets/images/people/brijesh.png'
+    }
+  ];
 }
